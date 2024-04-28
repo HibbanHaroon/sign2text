f9db2e4017ac6d8db4568f0412a36db80656d73c
HibbanHaroon <hibbanharoon@gmail.com>
2024-04-29 00:38:30 +0500

    Resolve error in routing

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,22 +1,23 @@
-import React from 'react'
-import Header from './Header'
-import HeroSection from './HeroSection'
-import Info from './Info'
-import Footer from './Footer'
+import React from "react";
+import Header from "./Header";
+import HeroSection from "./HeroSection";
+import Info from "./Info";
+import Footer from "./Footer";
 
 const Home = () => {
   return (
-    <div>    
-        <Header/>
-    <HeroSection/>
-    <div style={{ margin: '50px auto' }}>
-  <a className='main_pg_link' href="setup"><span>Click here to start Talking</span></a>
-</div>
-    <Info/>
-    <Footer/>
-</div>
+    <div>
+      <Header />
+      <HeroSection />
+      <div style={{ margin: "50px auto" }}>
+        <a className="main_pg_link" href="/setup">
+          <span>Click here to start Talking</span>
+        </a>
+      </div>
+      <Info />
+      <Footer />
+    </div>
+  );
+};
 
-  )
-}
-
-export default Home
+export default Home;
