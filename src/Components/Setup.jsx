2f4c1e06492d7db654cd000acde0df3d4a1f285e
HibbanHaroon <hibbanharoon@gmail.com>
2024-04-29 00:29:23 +0500

    Add Streamlit Links to Static and Dynamic Buttons

--- src/Components/Setup.jsx
+++ src/Components/Setup.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import Header from './Header';
-import Footer from './Footer';
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
 
 const Setup = () => {
   return (
     <div>
       <Header />
-      <div className='h-dvh flex flex-col justify-center items-center'>
-        <div className='mx-24'>
-          <div className='md:w-1/2 flex max-sm:gap-11 md:flex-row items-center'>
-            <a className='main_pg_link h-20 mb-4 md:mr-11' href="#"><span>Static</span></a>
-            <a className='main_pg_link h-20 mb-4' href="#"><span>Dynamic</span></a>
+      <div className="h-dvh flex flex-col justify-center items-center">
+        <div className="mx-24">
+          <div className="md:w-1/2 flex max-sm:gap-11 md:flex-row items-center">
+            <a
+              className="main_pg_link h-20 mb-4 md:mr-11"
+              href="http://localhost:8501/"
+            >
+              <span>Static</span>
+            </a>
+            <a className="main_pg_link h-20 mb-4" href="http://localhost:8501/">
+              <span>Dynamic</span>
+            </a>
           </div>
         </div>
       </div>
